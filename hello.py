f4175d7ad909aab7c978dd94df78156a83fead62
ikheu <iceking608@gmail.com>
2018-03-16 01:00:00 +0800

    chap6

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import os
+from threading import Thread
 from datetime import datetime
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
@@ -8,7 +9,8 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager, Shell
-from flask_migrate import Migrate, MigrateCommand 
+from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
 
 
 app = Flask(__name__)
@@ -19,13 +21,38 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['MAIL_SERVER'] = 'smtp.126.com'
+app.config['MAIL_PORT'] = 25
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'wbcsddc@126.com'#os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = 'what7078384'#os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'wbcsddc@126.com'
+app.config['FLASKY_ADMIN'] = '1311242552@qq.com'#os.environ.get('FLASK_ADMIN')
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 manager = Manager(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 manager.add_command('db', MigrateCommand)
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+    
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender = app.config['FLASKY_MAIL_SENDER'], recipients = [to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target = send_async_email, args = [app, msg])
+    thr.start()
+    return thr
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -75,6 +102,9 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user = user)
         else:
             session['known'] = True
         session['name'] = form.name.data
