86e8c873659f90171cebf71c43e61d3f4a0ae595
ikheu <iceking608@gmail.com>
2018-03-19 23:47:55 +0800

    chap8.5

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,5 @@
 from flask import Blueprint
-main = Blueprint('mian', __name__)
+
+main = Blueprint('main', __name__)
+
 from . import views, errors
--- config.py
+++ config.py
@@ -1,53 +1,46 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
-    SECRET_KEY = os.environ.get('SECURET_KEY') or 'hard to guess string'
-    SQLALCHEMY_COMMIT_ON_TEARDOWN  = True
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    #'Flasky Admin <flasky@example.com>'
-    FLASKY_MAIL_SENDER = 'wbcsddc@126.com'
-    #os.environ.get('FLASK_ADMIN')
-    FLASKY_ADMIN = '1311242552@qq.com'
-    
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+
     @staticmethod
     def init_app(app):
         pass
 
+
 class DevelopmentConfig(Config):
     DEBUG = True
-    MAIL_SERVER = 'smtp.126.com'
-    MAIL_PORT = 25
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
-    #os.environ.get('MAIL_USERNAME')
-    MAIL_USERNAME = 'wbcsddc@126.com'
-    #os.environ.get('MAIL_PASSWORD')
-    MAIL_PASSWORD = 'what7078384'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
-    
+
+
 class TestingConfig(Config):
-    TESTING  =True
+    TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
-    
+        'sqlite://'
+
+
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-        
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
-    
+
     'default': DevelopmentConfig
 }
-    
-    
-    
-    
-    
-    
-    
